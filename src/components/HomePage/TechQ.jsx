d034588111b0d379977bdb9c75f0efdc544e0129
Ziad Ali <ziadalikhalifa@gmail.com>
2019-12-14 16:46:33 +0200

    Converted Resources html file to jsx

--- src/components/HomePage/TechQ.jsx
+++ src/components/HomePage/TechQ.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import ResourcesPage from "../ResourcesPage/ResourcesPage";
 
 const TechQ = ({ isAuthenticated, authButtonMethod, user }) => {
   console.log(isAuthenticated);
   console.log(authButtonMethod);
   console.log(user);
 
-  return <>Hi</>;
+  return (
+    <>
+      <ResourcesPage />
+    </>
+  );
 };
 
 export default TechQ;
