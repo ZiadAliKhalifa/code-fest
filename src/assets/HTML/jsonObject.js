6b1e5699a507439d1dd3722a11da6be28f2eaef7
Ziad Ali <ziadalikhalifa@gmail.com>
2019-12-14 14:03:28 +0200

    Added the HTML supplied

---
+++ src/assets/HTML/jsonObject.js
@@ -0,0 +1,6 @@
+// const objectsList = [
+//     {
+//         name:""
+//     },
+//     {}
+// ]
---
+++ src/assets/HTML/tags-init.js
@@ -0,0 +1,24 @@
+var tagInput1 = new TagsInput({
+    selector: 'tag-input1',
+    duplicate: false,
+    max: 10
+});
+
+
+
+// // add Data programatically
+// tagInput1.addData(['PHP', 'JavaScript', 'CSS'])
+//     // or
+// tagInput1.addTag('React');
+
+// check if there are any erros in the String
+//tagInpu1.anyErrors('another tag');
+
+// you can destroy the plugin like this
+//tagInput1.destroy();
+
+// reinitialize it initialize it
+//tagInput1.init()
+
+// you can do all this by method chaining too
+//tagsInput1.init().addTag().addData().destroy().init()
